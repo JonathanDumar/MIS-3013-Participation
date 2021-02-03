3e9b77e1855b305aa7c6c84c76257f1343f8168d
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-02 18:33:21 -0600

    Completed example.

--- VariablesInputConversionFormatting/VariablesInputConversionFormatting/Program.cs
+++ VariablesInputConversionFormatting/VariablesInputConversionFormatting/Program.cs
@@ -6,7 +6,25 @@ namespace VariablesInputConversionFormatting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           string firstName, lastName;
+           double gpa;
+
+            Console.WriteLine("Please enter your first name >>")
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; // because we are being nice
+
+            string output = "Hello " + firstName + " " + lastName + "." + "We boosted your gpa to " + gpa.ToString("N3");
+
+            Console.WriteLine(output);
+
         }
     }
 }
