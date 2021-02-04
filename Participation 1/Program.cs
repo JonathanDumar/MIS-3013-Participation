d59da1079120aeb88cff6e6e6561ba3d421aa6ae
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-04 17:36:45 -0600

    Added constant.

---
+++ Participation 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Participation_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double MAGIC_NUMBER = 7.777; //local constant
+
+        }
+    }
+}
