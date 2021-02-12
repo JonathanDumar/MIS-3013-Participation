06d5fb3f545693bc2b505e4387d18fa2befd3d19
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-11 18:27:11 -0600

    Basic structure added.

---
+++ CoinToss/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string Answer;
+            
+
+            Console.WriteLine("Heads or Tails?");
+            Answer = Console.ReadLine();
+
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 3);
+
+
+            if (randomNbr == 1)
+            {
+               Console.WriteLine(Answer = "The coin landed on Heads");
+            }
+            else
+            {
+               Console.WriteLine(Answer = "The coin landed on Tails");
+            }
+
+            Console.WriteLine("The coin landed on "+ randomNbr);
+
+        }
+    }
+}
