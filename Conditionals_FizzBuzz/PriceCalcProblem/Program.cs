463cb9019dcdc5a0a69226ab246e3700c428f265
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-18 18:08:59 -0600

    In class example Feb. 18

---
+++ Conditionals_FizzBuzz/PriceCalcProblem/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace PriceCalcProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles;
+            double weight;
+            double quote;
+            double hazardousCost;
+            double netTotal;
+            double discount;
+            double total;
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please enter the number of miles >>");
+                answer = Console.ReadLine();
+                //miles = Convert.ToDouble(Console.ReadLine());
+                bool isSuccessfull = double.TryParse(answer, out miles);
+
+                if (isSuccessfull == false)
+                {
+                    Console.WriteLine("Your number is valid");
+
+                    //return;
+                    Environment.Exit(-1);
+                }
+
+                Console.WriteLine("Please enter the weight of your shipment in lbs >>");
+                weight = Convert.ToDouble(Console.ReadLine());
+                //miles = Convert.ToDouble(Console.ReadLine());
+                //bool isSuccessfull = double.TryParse(answer, out weight);
+
+                if (double.TryParse(answer, out weight) == false)
+                {
+                    Console.WriteLine("Your number is valid");
+
+                    //return;
+                    Environment.Exit(-2);
+                }
+
+                Console.WriteLine("Does your shipment contain hazardous material? yes or no?");
+                answer = Console.ReadLine();
+
+                quote = 0.55 * miles * 0.73 * weight;
+
+                if (answer.ToLower() == "yes")
+                {
+                    hazardousCost = 0.15 * weight;
+                }
+                else
+                {
+                    hazardousCost = 0;
+                }
+
+                netTotal = quote + hazardousCost;
+                discount = 0;
+
+                if (miles < 150 && weight > 500)
+                {
+                    discount = 0.10 * netTotal;
+                }
+
+                total = netTotal - discount;
+
+                Console.WriteLine("Quote:\t\t" + quote.ToString("C"));
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hazardous Cost:\t" + hazardousCost.ToString("C"));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Discount:\t\t" + discount.ToString("C"));
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Total:\t\t" + total.ToString("C"));
+
+                Console.WriteLine("Do you want to enter another shipment? yes or no? >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower = "yes");
+
+            
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
