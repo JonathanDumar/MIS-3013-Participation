a30165dbaf0626c96a35141a4c79eecb76a0fd78
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-23 18:20:21 -0600

    Recording initial user inputs.

---
+++ Loops_GuessANumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loops_GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowbound;
+            int highbound;
+
+            Console.WriteLine("Input your lower value >>");
+            lowbound = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input your higher value >>");
+            highbound = Convert.ToInt32(Console.ReadLine());
+
+
+            
+        }
+    }
+}
