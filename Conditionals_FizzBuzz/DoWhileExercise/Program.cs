b39e7ea8369feb6a1a477f71f04b4cf84226304f
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-23 18:04:03 -0600

    Lucky Number example.

---
+++ Conditionals_FizzBuzz/DoWhileExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DoWhileExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            int LuckyNumber;
+
+            
+            
+            Console.WriteLine("Please enter your lucky number >>");
+            answer = Console.ReadLine();
+
+            while (int.TryParse(answer, out LuckyNumber) == false)
+            {
+                Console.WriteLine("You entered an invalid integer. Please try again");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine("You entered a valid number!");
+
+        }
+    }
+}
