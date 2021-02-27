a5fb9740a49ec684661ae3759592573392789fa0
Jonathan Dumar <jonathan.dumar@ou.edu>
2021-02-26 18:25:20 -0600

    Just to be safe.

--- Loops_GuessANumber/Program.cs
+++ Loops_GuessANumber/Program.cs
@@ -8,15 +8,34 @@ namespace Loops_GuessANumber
         {
             int lowbound;
             int highbound;
+            int answer;
+            int randomNbr;
 
-            Console.WriteLine("Input your lower value >>");
-            lowbound = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Input your lower value >>");
+                lowbound = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input your higher value >>");
-            highbound = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input your higher value >>");
+                highbound = Convert.ToInt32(Console.ReadLine());
 
+                Random rnd = new Random();
+                randomNbr = rnd.Next(lowbound, highbound + 1);
 
-            
+                Console.WriteLine("Guess the random number between your lower and higher values >>");
+                answer = Convert.ToInt32(Console.ReadLine());
+
+                if (answer == randomNbr)
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly.");
+
+                }
+                else
+                {
+                    Console.WriteLine("You guessed incorrectly. Try again.");
+                }
+
+            } while (answer != randomNbr);
         }
     }
 }
